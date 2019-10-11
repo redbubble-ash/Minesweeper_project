92d05540473d5b4a52427d2863d4d67e4936a514
Your Name <xiejing418@gmail.com>
2019-10-10 23:04:02 -0700

    cleared my comments

--- Bomb.cs
+++ Bomb.cs
@@ -131,39 +131,5 @@ namespace Minesweeper_Project
                     return CellState.Empty;
             }
         }
-
-        //public static void PrintBoard()
-        //{
-        //    Console.Clear();
-
-        //    Console.WriteLine($" {GetNextEnum(state[0, 0])} | {GetNextEnum(state[0, 1])} | {GetNextEnum(state[0, 2])} || {GetNextEnum(state[0, 3])} || {GetNextEnum(state[0, 4])} || {GetNextEnum(state[0, 5])} || {GetNextEnum(state[0, 6])} || {GetNextEnum(state[0, 7])} || {GetNextEnum(state[0, 8])} || {GetNextEnum(state[0, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[1, 0])} | {GetNextEnum(state[1, 1])} | {GetNextEnum(state[1, 2])} || {GetNextEnum(state[1, 3])} || {GetNextEnum(state[1, 4])} || {GetNextEnum(state[1, 5])} || {GetNextEnum(state[1, 6])} || {GetNextEnum(state[1, 7])} || {GetNextEnum(state[1, 8])} || {GetNextEnum(state[1, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[2, 0])} | {GetNextEnum(state[2, 1])} | {GetNextEnum(state[2, 2])} || {GetNextEnum(state[2, 3])} || {GetNextEnum(state[2, 4])} || {GetNextEnum(state[2, 5])} || {GetNextEnum(state[2, 6])} || {GetNextEnum(state[2, 7])} || {GetNextEnum(state[2, 8])} || {GetNextEnum(state[2, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[3, 0])} | {GetNextEnum(state[3, 1])} | {GetNextEnum(state[3, 2])} || {GetNextEnum(state[3, 3])} || {GetNextEnum(state[3, 4])} || {GetNextEnum(state[3, 5])} || {GetNextEnum(state[3, 6])} || {GetNextEnum(state[3, 7])} || {GetNextEnum(state[3, 8])} || {GetNextEnum(state[3, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[4, 0])} | {GetNextEnum(state[4, 1])} | {GetNextEnum(state[4, 2])} || {GetNextEnum(state[4, 3])} || {GetNextEnum(state[4, 4])} || {GetNextEnum(state[4, 5])} || {GetNextEnum(state[4, 6])} || {GetNextEnum(state[4, 7])} || {GetNextEnum(state[4, 8])} || {GetNextEnum(state[4, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[5, 0])} | {GetNextEnum(state[5, 1])} | {GetNextEnum(state[5, 2])} || {GetNextEnum(state[5, 3])} || {GetNextEnum(state[5, 4])} || {GetNextEnum(state[5, 5])} || {GetNextEnum(state[5, 6])} || {GetNextEnum(state[5, 7])} || {GetNextEnum(state[5, 8])} || {GetNextEnum(state[5, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[6, 0])} | {GetNextEnum(state[6, 1])} | {GetNextEnum(state[6, 2])} || {GetNextEnum(state[6, 3])} || {GetNextEnum(state[6, 4])} || {GetNextEnum(state[6, 5])} || {GetNextEnum(state[6, 6])} || {GetNextEnum(state[6, 7])} || {GetNextEnum(state[6, 8])} || {GetNextEnum(state[6, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[7, 0])} | {GetNextEnum(state[7, 1])} | {GetNextEnum(state[7, 2])} || {GetNextEnum(state[7, 3])} || {GetNextEnum(state[7, 4])} || {GetNextEnum(state[7, 5])} || {GetNextEnum(state[7, 6])} || {GetNextEnum(state[7, 7])} || {GetNextEnum(state[7, 8])} || {GetNextEnum(state[7, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[8, 0])} | {GetNextEnum(state[8, 1])} | {GetNextEnum(state[8, 2])} || {GetNextEnum(state[8, 3])} || {GetNextEnum(state[8, 4])} || {GetNextEnum(state[8, 5])} || {GetNextEnum(state[8, 6])} || {GetNextEnum(state[8, 7])} || {GetNextEnum(state[8, 8])} || {GetNextEnum(state[8, 9])}");
-        //    Console.WriteLine("---+---+---+---+---+---+---+---+---+---");
-
-        //    Console.WriteLine($" {GetNextEnum(state[9, 0])} | {GetNextEnum(state[9, 1])} | {GetNextEnum(state[9, 2])} || {GetNextEnum(state[9, 3])} || {GetNextEnum(state[9, 4])} || {GetNextEnum(state[9, 5])} || {GetNextEnum(state[9, 6])} || {GetNextEnum(state[9, 7])} || {GetNextEnum(state[9, 8])} || {GetNextEnum(state[9, 9])}");
-        //}
     }
 }
