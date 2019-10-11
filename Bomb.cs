da476b286fc0e7e6f52f4429be28a552bcc1bc8a
Your Name <xiejing418@gmail.com>
2019-10-11 00:20:11 -0700

    fixed the bug of assigning numbers in the board

--- Bomb.cs
+++ Bomb.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace Minesweeper_Project
 {
-    public enum CellState { Empty, M1, M2, M3, M4, M5, M6, M7, M8, M9, Bo };
+    public enum CellState { Em, M1, M2, M3, M4, M5, M6, M7, M8, M9, Bo };
 
     public class Bomb
     {
@@ -44,9 +44,10 @@ namespace Minesweeper_Project
                         {
                             continue;
                         }
-                        GetNextEnum(state[nearBombX, nearBombY]);
-                        GetNextEnum(state[nearBombX + 1, nearBombY]);
-                        GetNextEnum(state[nearBombX + 2, nearBombY]);
+
+                        state[nearBombX, nearBombY] = GetNextEnum(state[nearBombX, nearBombY]);
+                        state[nearBombX + 1, nearBombY] = GetNextEnum(state[nearBombX + 1, nearBombY]);
+                        state[nearBombX + 2, nearBombY] = GetNextEnum(state[nearBombX + 2, nearBombY]);
                     }
                 }
                 else
@@ -55,46 +56,31 @@ namespace Minesweeper_Project
                 }
             }
 
-            //Console.WriteLine(GetNextEnum(state[3, 3]));
-
             //Print the board
             Console.Clear();
 
-            Console.WriteLine($" {GetNextEnum(state[0, 0])} | {GetNextEnum(state[0, 1])} | {GetNextEnum(state[0, 2])} | {GetNextEnum(state[0, 3])} | {GetNextEnum(state[0, 4])} | {GetNextEnum(state[0, 5])} | {GetNextEnum(state[0, 6])} | {GetNextEnum(state[0, 7])} | {GetNextEnum(state[0, 8])} | {GetNextEnum(state[0, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[1, 0])} | {GetNextEnum(state[1, 1])} | {GetNextEnum(state[1, 2])} | {GetNextEnum(state[1, 3])} | {GetNextEnum(state[1, 4])} | {GetNextEnum(state[1, 5])} | {GetNextEnum(state[1, 6])} | {GetNextEnum(state[1, 7])} | {GetNextEnum(state[1, 8])} | {GetNextEnum(state[1, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[2, 0])} | {GetNextEnum(state[2, 1])} | {GetNextEnum(state[2, 2])} | {GetNextEnum(state[2, 3])} | {GetNextEnum(state[2, 4])} | {GetNextEnum(state[2, 5])} | {GetNextEnum(state[2, 6])} | {GetNextEnum(state[2, 7])} | {GetNextEnum(state[2, 8])} | {GetNextEnum(state[2, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[3, 0])} | {GetNextEnum(state[3, 1])} | {GetNextEnum(state[3, 2])} | {GetNextEnum(state[3, 3])} | {GetNextEnum(state[3, 4])} | {GetNextEnum(state[3, 5])} | {GetNextEnum(state[3, 6])} | {GetNextEnum(state[3, 7])} | {GetNextEnum(state[3, 8])} | {GetNextEnum(state[3, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[4, 0])} | {GetNextEnum(state[4, 1])} | {GetNextEnum(state[4, 2])} | {GetNextEnum(state[4, 3])} | {GetNextEnum(state[4, 4])} | {GetNextEnum(state[4, 5])} | {GetNextEnum(state[4, 6])} | {GetNextEnum(state[4, 7])} | {GetNextEnum(state[4, 8])} | {GetNextEnum(state[4, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[5, 0])} | {GetNextEnum(state[5, 1])} | {GetNextEnum(state[5, 2])} | {GetNextEnum(state[5, 3])} | {GetNextEnum(state[5, 4])} | {GetNextEnum(state[5, 5])} | {GetNextEnum(state[5, 6])} | {GetNextEnum(state[5, 7])} | {GetNextEnum(state[5, 8])} | {GetNextEnum(state[5, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[6, 0])} | {GetNextEnum(state[6, 1])} | {GetNextEnum(state[6, 2])} | {GetNextEnum(state[6, 3])} | {GetNextEnum(state[6, 4])} | {GetNextEnum(state[6, 5])} | {GetNextEnum(state[6, 6])} | {GetNextEnum(state[6, 7])} | {GetNextEnum(state[6, 8])} | {GetNextEnum(state[6, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[7, 0])} | {GetNextEnum(state[7, 1])} | {GetNextEnum(state[7, 2])} | {GetNextEnum(state[7, 3])} | {GetNextEnum(state[7, 4])} | {GetNextEnum(state[7, 5])} | {GetNextEnum(state[7, 6])} | {GetNextEnum(state[7, 7])} | {GetNextEnum(state[7, 8])} | {GetNextEnum(state[7, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[8, 0])} | {GetNextEnum(state[8, 1])} | {GetNextEnum(state[8, 2])} | {GetNextEnum(state[8, 3])} | {GetNextEnum(state[8, 4])} | {GetNextEnum(state[8, 5])} | {GetNextEnum(state[8, 6])} | {GetNextEnum(state[8, 7])} | {GetNextEnum(state[8, 8])} | {GetNextEnum(state[8, 9])}");
-            Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-
-            Console.WriteLine($" {GetNextEnum(state[9, 0])} | {GetNextEnum(state[9, 1])} | {GetNextEnum(state[9, 2])} | {GetNextEnum(state[9, 3])} | {GetNextEnum(state[9, 4])} | {GetNextEnum(state[9, 5])} | {GetNextEnum(state[9, 6])} | {GetNextEnum(state[9, 7])} | {GetNextEnum(state[9, 8])} | {GetNextEnum(state[9, 9])}");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j < 9)
+                    {
+                        Console.Write($" {state[i, j]} |");
+                    }
+                    else
+                    {
+                        Console.Write($" {state[i, j]} \n");
+                    }
+                }
+                Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
+            }
         }
 
         public static CellState GetNextEnum(CellState state)
         {
             switch (state)
             {
-                case CellState.Empty:
+                case CellState.Em:
                     return CellState.M1;
 
                 case CellState.M1:
@@ -128,7 +114,7 @@ namespace Minesweeper_Project
                     return CellState.Bo;
 
                 default:
-                    return CellState.Empty;
+                    return CellState.Em;
             }
         }
     }
---
+++ Play_Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minesweeper_Project
+{
+    internal class Play_Game
+    {
+        private void ClickCells(int i, int j)
+        {
+            //if (i < 0 || i >= 10 || j < 0 || j >= 10)
+            //    return;
+            //if (Bomb.state[i, j] == CellState.Bo)
+            //{
+            //    Bomb.state[i, j] = -1;
+
+            //    ClickCells(i - 1, j - 1);
+            //    ClickCells(i - 1, j);
+            //    ClickCells(i - 1, j + 1);
+            //    ClickCells(i + 1, j - 1);
+            //    ClickCells(i + 1, j);
+            //    ClickCells(i + 1, j + 1);
+            //    ClickCells(i, j + 1);
+            //    ClickCells(i, j - 1);
+            //}
+
+            //buttons[i, j].Unseal();//
+            //map[i, j] = -1;
+        }
+    }
+}
