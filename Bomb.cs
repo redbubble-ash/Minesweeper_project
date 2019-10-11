fe6802e2501fa30440f635f48ec91c2069bd74cc
Your Name <xiejing418@gmail.com>
2019-10-11 01:55:02 -0700

    cleaned up some comments

--- Bomb.cs
+++ Bomb.cs
@@ -55,25 +55,6 @@ namespace Minesweeper_Project
                     i--;
                 }
             }
-
-            //Print the board
-            //Console.Clear();
-
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    for (int j = 0; j < 10; j++)
-            //    {
-            //        if (j < 9)
-            //        {
-            //            Console.Write($" {state[i, j]} |");
-            //        }
-            //        else
-            //        {
-            //            Console.Write($" {state[i, j]} \n");
-            //        }
-            //    }
-            //    Console.WriteLine("----+----+----+----+----+----+----+----+----+----");
-            //}
         }
 
         public static CellState GetNextEnum(CellState state)
